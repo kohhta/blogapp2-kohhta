bf34c88ad5d9e37d1bb39e774eb50836f9b2fd2c
kota_koyama <kouta103177@gmail.com>
2021-01-22 18:19:07 +0900

    日付の表記を動的に

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 class Article < ApplicationRecord
+  # バリデーションを追加
   validates :title, presence: true
   validates :content, presence: true
+
+  # 投稿日を日本語化
+  def display_created_at
+    I18n.l(self.created_at, format: :default)
+  end
+
 end
