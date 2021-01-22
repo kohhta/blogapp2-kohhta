53f228040080fc7a5b97e9a5c1e127f7478bab8e
kota_koyama <kouta103177@gmail.com>
2021-01-22 19:11:31 +0900

    prybybug,Haml,anotate導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Article < ApplicationRecord
   # バリデーションを追加
   validates :title, presence: true
