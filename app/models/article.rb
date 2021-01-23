aea94e8159acfcb1d8aa70a3c9ef1dfd7dc244fe
kota_koyama <kouta103177@gmail.com>
2021-01-23 15:13:33 +0900

    validation length文字の長さ,uniqueness を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,7 +11,14 @@
 class Article < ApplicationRecord
   # バリデーションを追加
   validates :title, presence: true
+  #length文字の長さ
+  validates :title, length: { minimum: 2, maximum: 100 }
+
   validates :content, presence: true
+  #length文字の長さ
+  validates :content, length: { minimum: 2 }
+  #uniqueness 同じ文章はNGのValidation
+  validates :content, uniqueness: true
 
   # 投稿日を日本語化
   def display_created_at
