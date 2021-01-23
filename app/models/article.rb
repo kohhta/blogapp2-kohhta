4463e0942f33eedaf58632e9962a44c76aa94006
kota_koyama <kouta103177@gmail.com>
2021-01-23 15:27:28 +0900

    titleに先頭に＠がついたらNGのvalidates、自分に作ったvalidates,titleとcontentの文字数合計

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,6 +13,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
   #length文字の長さ
   validates :title, length: { minimum: 2, maximum: 100 }
+  #titleに先頭に＠がついたらNGのvalidates
+  validates :title, format: { with: /\A(?!\@)/ }
 
   validates :content, presence: true
   #length文字の長さ
@@ -20,8 +22,20 @@ class Article < ApplicationRecord
   #uniqueness 同じ文章はNGのValidation
   validates :content, uniqueness: true
 
+  #自分に作ったvalidates,titleとcontentの文字数合計
+  validate :validate_title_and_content_length
+
   # 投稿日を日本語化
   def display_created_at
     I18n.l(created_at, format: :default)
   end
+
+  private 
+  def validate_title_and_content_length
+    word_count = self.title.length + self.content.length
+    unless word_count > 5
+      errors.add(:content, '5文字以上にしてください！！')
+    end
+  end
+  
 end
