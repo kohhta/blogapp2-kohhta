5dbdd0b57a02d875732fe534e7e7500e6d53fd9d
kota_koyama <kouta103177@gmail.com>
2021-01-26 18:21:44 +0900

    updateアクション,consoleで確認

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,4 @@
 class ProfilesController < ApplicationController
-
   before_action :authenticate_user!
 
   def show
@@ -10,4 +9,24 @@ class ProfilesController < ApplicationController
     @profile = current_user.build_profile
   end
 
+  def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィール更新！'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :nickname,
+      :introduction,
+      :gender,
+      :birthday,
+      :subscribed
+    )
+  end
 end
